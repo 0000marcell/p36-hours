47dda7e1f88839af90078f6490463b6e50286bf6
Marcell Monteiro Cruz <0000marcell@gmail.com>
2018-03-05 10:44:23 -0300

    implementing universal clock with services

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,11 @@
 import PouchDB from 'pouchdb';
 import { Adapter } from 'ember-pouch';
 
-let db = new PouchDB('p36-hours3', { adapter: 'idb'});
+let db = new PouchDB('p36-hours');
 
 //db.destroy();
+
+//window.localStorage.clear();
 
 export default Adapter.extend({
   db: db
