7968e8c6b7461eefa662a880aebf7f8ac17c58c5
Marcell Monteiro Cruz <0000marcell@gmail.com>
2018-03-22 11:53:04 -0300

    properly loading fake data #5

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,18 @@
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
+import { get } from '@ember/object';
+import fakeData from '../p36-hours/fake-data';
+import mock from '../p36-hours/mock';
 
 export default Route.extend({
   async model(){
+    let tasks = await this.store.findAll('task');
+    if(!get(tasks, 'length'))
+      tasks = await mock.constructDbFromObj(this.store, 
+        fakeData);
+
     return hash({
-      tasks: this.store.findAll('task'),
+      tasks: tasks,
       pomodoros: this.store.findAll('pomodoro'),
       tags: this.store.findAll('tag')
     });
--- app/routes/clock.js
+++ app/routes/clock.js
@@ -1,15 +1,9 @@
 import Route from '@ember/routing/route';
 import filter from '../p36-hours/filters';
-import fakeData from '../p36-hours/fake-data';
-import mock from '../p36-hours/mock';
-import { get } from '@ember/object';
 
 export default Route.extend({
   async setupController(controller, model){
     this._super(...arguments);
-    if(!get(model.tasks, 'length'))
-      model.tasks = await mock.constructDbFromObj(this.store, 
-        fakeData);
     filter.rootTasks(model.tasks).then((tasks) => {
       controller.set('filteredTasks', tasks);
       controller.set('_tabTasks', tasks);
